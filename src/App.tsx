a57c27ea44947cc00af27a0e6cea37b9bc1e86e0
alexander-rivaldy <alexanderrivaldy@gmail.com>
2020-05-26 00:36:03 +1000

    implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import QRCode from 'qrcode';
+
 import './App.css';
 
-function App() {
+const App: React.FunctionComponent = () => {
+  const [error, setError] = useState(false);
+  const [qrcodeValue, setQrcodeValue] = useState('');
+  const [qrcodeImage, setQrcodeImage] = useState('');
+
+  const generateQRCode = async (text: string) => {
+    const url = await QRCode.toDataURL(text, { margin: 1, scale: 10 });
+    console.log(text);
+    console.log(url);
+    return url;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="input-container">
+          {error && <p className="error-message">Put in a value!!!</p>}
+          <label>Enter the website url (or any text at all!) here:</label>
+          <input
+            type="text"
+            id="qrcodeValue"
+            value={qrcodeValue}
+            onChange={(e) => setQrcodeValue(e.target.value)}
+          ></input>
+
+          <button
+            onClick={async (e: any) => {
+              const qrcodeValueElement: any = document.getElementById(
+                'qrcodeValue'
+              );
+              const value: string = qrcodeValueElement.value;
+              if (value) {
+                setQrcodeImage(await generateQRCode(qrcodeValueElement.value));
+                setError(false);
+              } else {
+                setError(true);
+              }
+            }}
+          >
+            GENERATE!!!
+          </button>
+        </div>
+        <div className="qrcode-container">
+          <img src={qrcodeImage}></img>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
